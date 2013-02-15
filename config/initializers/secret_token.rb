bc789565e819181d5b5e90b9eb26bd329bfd6910
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-15 21:03:42 +0900

    add: first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spinetest::Application.config.secret_token = '6f376e6cb1c9184cec89fe23053434db6367f3d3b906e1833cfe22f012d99e3e009f004b62468b7a2a758be50f945c169e5fc89c3ef8891bfe51b731d52e57f0'
